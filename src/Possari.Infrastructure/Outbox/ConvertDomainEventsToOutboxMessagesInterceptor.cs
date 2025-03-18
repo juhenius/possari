6cdc87d4657fdb027543cb0b6cb358f794754824
Jari Helenius <jpjhel+github@gmail.com>
2025-03-18 20:27:25 +0200

    Trigger outbox processing after domain events have been raised

--- src/Possari.Infrastructure/Outbox/ConvertDomainEventsToOutboxMessagesInterceptor.cs
+++ src/Possari.Infrastructure/Outbox/ConvertDomainEventsToOutboxMessagesInterceptor.cs
@@ -2,14 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Newtonsoft.Json;
 using Possari.Domain.Primitives;
+using Quartz;
 
 namespace Possari.Infrastructure.Outbox;
 
-public sealed class ConvertDomainEventsToOutboxMessagesInterceptor : SaveChangesInterceptor
+public sealed class ConvertDomainEventsToOutboxMessagesInterceptor(ISchedulerFactory schedulerFactory) : SaveChangesInterceptor
 {
+  private readonly ISchedulerFactory _schedulerFactory = schedulerFactory;
+
   public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
     DbContextEventData eventData,
-    InterceptionResult<int> result,
+    InterceptionResult<int> interceptionResult,
     CancellationToken cancellationToken = default
   )
   {
@@ -18,12 +21,28 @@ public sealed class ConvertDomainEventsToOutboxMessagesInterceptor : SaveChanges
       CreateOutboxMessagesFromDomainEvents(eventData.Context);
     }
 
-    return base.SavingChangesAsync(eventData, result, cancellationToken);
+    return base.SavingChangesAsync(eventData, interceptionResult, cancellationToken);
+  }
+
+  public override async ValueTask<int> SavedChangesAsync(
+    SaveChangesCompletedEventData eventData,
+    int result,
+    CancellationToken cancellationToken = default)
+  {
+    var addedMessages = eventData?.Context?.ChangeTracker.Entries<OutboxMessage>().Count() ?? 0;
+    if (addedMessages > 0)
+    {
+      var scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
+      var jobKey = new JobKey(nameof(ProcessOutboxMessagesJob));
+      await scheduler.TriggerJob(jobKey, cancellationToken);
+    }
+
+    return result;
   }
 
   private static void CreateOutboxMessagesFromDomainEvents(DbContext context)
   {
-    context.Set<OutboxMessage>().AddRange(context.ChangeTracker
+    List<OutboxMessage> messages = [.. context.ChangeTracker
       .Entries<AggregateRoot>()
       .SelectMany(x => x.Entity.PopDomainEvents())
       .Select(domainEvent => new OutboxMessage
@@ -35,6 +54,11 @@ public sealed class ConvertDomainEventsToOutboxMessagesInterceptor : SaveChanges
         {
           TypeNameHandling = TypeNameHandling.All
         }),
-      }).ToList());
+      })];
+
+    if (messages.Count > 0)
+    {
+      context.Set<OutboxMessage>().AddRange(messages);
+    }
   }
 }
